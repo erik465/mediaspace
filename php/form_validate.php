6106a8a6fba14c457e7c62fd92f2e801435f712f
IaroslavGalas <74241446+IaroslavGalas@users.noreply.github.com>
2021-04-28 19:55:01 +0200

    Update form_validate.php

--- php/form_validate.php
+++ php/form_validate.php
@@ -16,9 +16,11 @@ $to = 'mediaspace.es@gmail.com';
 $message = "Nombre: ".$name ."\n Compañía: ".$business ."\nTelefono: " . $tel . "\nLocacion: ".$loc. "\n Altura: " . $he . "\n Anchura: ".$wi."\n Pitch: ". $pitch . "\n Tipo: " . $type;
 $subject = 'Presupuesto LED';
 $subject = "=?utf-8?B?".base64_encode($subject)."?=";
-$headers = "From: $from \r\n Reply to: $from \r\n Content-type: text/plain; charset=utf-0 \r\n";
+$headers = "From: $from \r\n Reply to: $from \r\n Content-type: text/plain; charset=utf-8 \r\n";
 
 mail($to, $subject, $message, $headers);
+header("Location: https://mediaspace.es/");
+exit;
 
 
 ?>
